64830574902ec751d9302fa35141d72f5a84ee5f
David <dpg32@bath.ac.uk>
2019-11-11 11:53:23 +0000

    added scheduler

--- webapp.py
+++ webapp.py
@@ -4,6 +4,7 @@ import time
 import atexit
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
 from flask import Flask, redirect, url_for, request
 from flask import render_template
 
@@ -11,20 +12,21 @@ api = __import__("api-accessor")
 
 
 app = Flask(__name__)
-scheduler = BackgroundScheduler() # initialise scheduler
-scheduler.start() # start scheduler
-atexit.register(lambda: scheduler.shutdown()) # kill when exiting app
 
-# EXAMPLE SCHEDULER TASK
-# def print_date_time():
-#     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
-#
-# scheduler.add_job(func=print_date_time, trigger="interval", seconds=3)
-# print(api.main(1352))
 
 ### Globals
+data_dict = {}
+apiTime = 15
 
+### Functions
+def getApiData(id):
+	print("getting data")
+	data_dict.update(api.main(id))
 
+def listener(event):
+	if not event.exception:
+		print("successfully got data")
+		print(len(data_dict.keys()))
 
 @app.route('/')
 def hello_world():
@@ -32,4 +34,15 @@ def hello_world():
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = 3500, debug = True)
+		scheduler = BackgroundScheduler() # initialise scheduler
+		scheduler.add_listener(listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+		scheduler.add_job(func=getApiData,trigger="interval",args=[1352],seconds=apiTime*60*60)
+		scheduler.start() # start scheduler
+		atexit.register(lambda: scheduler.shutdown()) # kill when exiting app
+		app.run(host = "0.0.0.0", port = 3500, debug = True)	
+
+		print("successfully started server")
+		for job in cron.get_jobs():
+			job.modify(next_run_time=datetime.now())
+
+
