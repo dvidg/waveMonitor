fdd30464fcb017567161d46ce3fa8aebcb54b9a9
David <dpg32@bath.ac.uk>
2019-11-11 12:44:33 +0000

    fixed getting of data into main webapp file

--- static/scripts.js
+++ static/scripts.js
@@ -1,15 +1,6 @@
-// scripts.js
-/*  EXAMPLE SCRIPT
-// a couple of different backgrounds to style the shop
-var background1 = 'black';
-var background2 = 'firebrick';
-
-// this lets us toggle the background state
-var color = true;
-
-// every 1 second, switch the background color, alternating between the two styles
 setInterval(function () {
-  document.body.style.backgroundColor = (color ? background1 : background2)
-  color = !color;
+	document.write(Date.now())	
 }, 1000);
-*/
+
+
+
