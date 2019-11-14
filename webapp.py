06de031b53785f953852c6a9de03c84361796ec1
David <dpg32@bath.ac.uk>
2019-11-14 10:54:57 +0000

    moved styling of arrows to css file

--- webapp.py
+++ webapp.py
@@ -30,8 +30,6 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
 									'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
-
-
 def getApiData(id):
 	data_dict.clear()
 	data_dict.update(api.main(id))
