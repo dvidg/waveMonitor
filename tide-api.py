9717a2e27564886ed59b508b680ee372ccaf4c53
David <dpg32@bath.ac.uk>
2019-11-15 16:24:32 +0000

    added tide api

--- tide-api.py
+++ tide-api.py
@@ -4,8 +4,34 @@ API: https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/<URL>/TidalEv
 ID: Porthcawl 0512 | Woolacombe 0535 (Ilfracombe closest)
 Visual search site: http://www.ukho.gov.uk/Easytide/easytide/SelectPort.aspx
 """
+import http.client
+import urllib.request
+import urllib.parse
+import urllib.error
+import base64
+
+
+def makeRequest():
+	# Request header to include key
+	headers = {
+			'Ocp-Apim-Subscription-Key': 'c4249a64c1064f4d893dcb19bee78667',
+	}
+
+	# Request parameters
+	params = urllib.parse.urlencode({
+			'duration': '7',
+	})
+
+	try:
+			conn = http.client.HTTPSConnection('admiraltyapi.azure-api.net')
+			conn.request("GET", "/uktidalapi/api/V1/Stations/0512/TidalEvents?%s" % params, "{body}", headers)
+			response = conn.getresponse()
+			data = response.read()
+			print(data)
+			conn.close()
+	except Exception as e:
+			print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 
 if __name__ == '__main__':
-	
-
+  makeRequest()
