447c8c75eba3e4a86282ac61b215f6e3c3ea98d0
David <dpg32@bath.ac.uk>
2019-11-15 16:16:55 +0000

    fixed getData 2 args error

--- tide-api.py
+++ tide-api.py
@@ -1,11 +1,11 @@
 """
 API docu:https://admiraltyapi.portal.azure-api.net/docs/services/uk-tidal-api/operations/Stations_GetStation
-API: https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/{stationId}
+API: https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/<URL>/TidalEvents
 ID: Porthcawl 0512 | Woolacombe 0535 (Ilfracombe closest)
 Visual search site: http://www.ukho.gov.uk/Easytide/easytide/SelectPort.aspx
 """
 
 
 if __name__ == '__main__':
-
+	
 
