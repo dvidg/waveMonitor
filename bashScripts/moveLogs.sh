0f759097f74541b9e892dbef8ac8de8d4ff91af7
David <david.gwynne@ntlworld.com>
2019-11-29 15:13:47 +0000

    fixed nohup writing

--- bashScripts/moveLogs.sh
+++ bashScripts/moveLogs.sh
@@ -3,14 +3,10 @@ if [ -s log.txt ]
 then
 	cp log.txt "pastLogs/log$(date +"%m-%d-%y").txt"
 	> log.txt
-else
-	echo "logs empty"
 fi
 
 if [ -s nohup.out ]
 then
 	cp nohup.out "pastLogs/nohup$(date +"%m-%d-%y").out"
 	> nohup.out
-else
-	echo "nohup empty"
 fi
